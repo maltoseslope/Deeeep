a20528d1647ca6791576df65427f099d1317bed6
Chenfeng Tu <chenfent@andrew.cmu.edu>
2019-11-14 16:24:08 -0500

    debugging model

--- Conv_LSTM_Conv.py
+++ Conv_LSTM_Conv.py
@@ -26,8 +26,20 @@ class Conv_LSTM_Conv(nn.Module):
 
     def forward(self, inputs): # The input to this network should be a batch of size 64 * 64
         feature_map = self.convs1(inputs) # out shape (16, 16, N)?
-        feature_map = torch.reshape(feature_map, (-1,))
+        feature_map = torch.squeeze(feature_map) # [N, 64. 64]
+
+        # reshape output to LSTM input
+        feature_map_flattened = torch.zeros((64, feature_map.shape[0], 64))  #(seq_len, batch, input_size)
+        count = 0
+        for i in range(8):
+            for j in range(8):
+                temp = feature_map[:, i*8:i*8 + 8,j*8:j*8 + 8].reshape((-1, 64))
+                temp2 = feature_map_flattened[count, :, :]
+                feature_map_flattened[count, :, :] = feature_map[:, i*8:i*8 + 8,j*8:j*8 + 8].reshape((-1, 64))
+                count += 1
         rnn_out = self.rnn(feature_map)[0]
+
+        # reshape back to cnn format
         out = self.convs2(rnn_out)
         predicted_label = self.linear(rnn_out)
         return out, predicted_label
--- Deeeep_image_forensics.py
+++ Deeeep_image_forensics.py
@@ -10,14 +10,15 @@ import pandas as pd
 
 from Conv_LSTM_Conv import Conv_LSTM_Conv
 
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+DEVICE = "cpu"# if torch.cuda.is_available() else "cpu"
 print(DEVICE)
 
+# load data and check data dimension
 datapath = '/home/chenfeng/Downloads/ProjectData/data.npz'
 data = np.load(datapath, allow_pickle=True)
-train_data = data['train_data']
-train_label = data['train_label']
-train_mask = data['train_mask']
+train_data = data['train_data']       # return 3-channel image patch
+train_label = data['train_label']     # if this patch is modified, return label 1, else return 0
+train_mask = data['train_mask']       # if the pixel is modified, return 255, else return 0
 val_data = data['val_data']
 val_label = data['val_label']
 val_mask = data['val_mask']
@@ -25,20 +26,30 @@ test_data = data['test_data']
 test_label = data['test_label']
 test_mask = data['test_mask']
 
+# swap dimensions to [N, C, H, W]
+train_data = np.swapaxes(train_data, 1, 3)
+train_data = np.swapaxes(train_data, 2, 3)
+val_data = np.swapaxes(val_data, 1, 3)
+val_data = np.swapaxes(val_data, 2, 3)
+test_data = np.swapaxes(test_data, 1, 3)
+test_data = np.swapaxes(test_data, 2, 3)
+
+
+print("train_data shape: ", train_data.shape)      # [N, 3, 64, 64]
+print("train_label shape: ", train_label.shape)    # [N,]
+print("train_mask shape: ", train_mask.shape)      # [N, 64. 64]
+
 class ForensicsDataset(Dataset):
-    def __init__(self, data, label):
+    def __init__(self, data, label, mask):
         self.data = data
         self.label = label
+        self.mask = mask
 
     def __getitem__(self, i):
-        pass
+        return torch.tensor(self.data[i, :, :, :]).float(), torch.tensor(self.label[i]).float(), torch.tensor(self.mask[i, :, :]).float()
 
     def __len__(self):
         return self.label.shape[0]
-
-# collate fn lets you control the return value of each batch
-def collate(seq_list):
-    pass
 
 def train_epoch(model, optimizer, train_loader, val_loader):
     model.train()
@@ -67,25 +78,27 @@ def train_epoch(model, optimizer, train_loader, val_loader):
     val_loss = 0
     # ...
 
+# Define model
 model = Conv_LSTM_Conv(3)
 model = model.to(DEVICE)
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-5)
-# optimizer = torch.optim.SGD(model.parameters(), lr=1e-3, momentum=0.5, weight_decay=1e-5)
-train_dataset = ForensicsDataset(train_data)
-val_dataset = ForensicsDataset(dev_data)
-train_loader = DataLoader(train_dataset, shuffle=True, batch_size=32, collate_fn=collate)
-val_loader = DataLoader(val_dataset, shuffle=False, batch_size=32, collate_fn=collate, drop_last=True)
+
+train_dataset = ForensicsDataset(train_data, train_label, train_mask)
+val_dataset = ForensicsDataset(val_data, val_label, val_mask)
+train_loader = DataLoader(train_dataset, shuffle=True, batch_size=32)
+val_loader = DataLoader(val_dataset, shuffle=False, batch_size=32, drop_last=True)
 
 
-# for i in range(30):
-#     train_epoch_packed(model, optimizer, train_loader, val_loader, decoder)
-#     print("Epoch ", i ," finished!")
-#     model_name = "trained_model_epoch"+str(i)
-#     torch.save(model, model_name)
+for i in range(30):
+    train_epoch(model, optimizer, train_loader, val_loader)
+    print("Epoch ", i ," finished!")
+    model_name = "trained_model_epoch"+str(i)
+    torch.save(model, model_name)
 
 
 
 
 # test
-
+# test_dataset = ForensicsDataset(test_data, test_label, test_mask)
+# test_loader = DataLoader(test_dataset, shuffle=False, batch_size=32, collate_fn=collate)
 
